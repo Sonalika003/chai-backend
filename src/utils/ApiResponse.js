aa1cbf5f16764f0eb1bf47b0640a8e2255490198
Sonalika <sonalikachaudhari43@gmail.com>
2025-06-24 15:18:30 +0530

    utils code discussion

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,15 @@
+class ApiResponse {
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode= statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+    }
+}
+
+// statuscode-->
+// 1.information response (100 - 199)
+// 2.successful response (200 - 299)
+// 3.redirection messages (300 - 399)
+// 4.client error response (400 - 499)
+// 5.server error response (500 - 599)
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// promises part-->
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+    Promise.resolve(requestHandler(req,res,next)).
+    catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+
+
+
+
+// try and catch part-->
+// const asyncHandler = (fn) => async(req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+        
+//     }
+// }
