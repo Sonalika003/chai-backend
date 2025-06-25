9417f6459aeb1342648d7a743822afa976149ff0
Sonalika <sonalikachaudhari43@gmail.com>
2025-06-25 12:46:36 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//import router
+import userRouter from './routes/user.routes.js'
+
+//router declaration
+app.use("/api/v1/users", userRouter) //userRouter activate ho jaega
+
 export { app }
